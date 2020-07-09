e21df80d000c3342096edf6f07b683d1aedffc1c
James Bale <james@buzzinteractive.co.uk>
2020-07-09 17:50:57 +0100

    fix: Fixed select clickable area
    There was an issue with the height of the select. This change allows
    the user to click on the whole button to activate the select

--- src/components/Book/BookShelfSelector.js
+++ src/components/Book/BookShelfSelector.js
@@ -5,13 +5,11 @@ import PropTypes from 'prop-types';
 const BookShelfSelector = ({currentShelf, shelves}) => {
 
   return (
-    <div className="book-shelf-changer">
-      <select defaultValue={currentShelf}>
-        <option value="move" disabled>Move to...</option>
-        {shelves.map((shelf) => (<option key={shelf.id} value={shelf.id}>{shelf.name}</option>))}
-        <option value="none">None</option>
-      </select>
-    </div>
+    <select defaultValue={currentShelf}>
+      <option value="move" disabled>Move to...</option>
+      {shelves.map((shelf) => (<option key={shelf.id} value={shelf.id}>{shelf.name}</option>))}
+      <option value="none">None</option>
+    </select>
   );
 };
 
