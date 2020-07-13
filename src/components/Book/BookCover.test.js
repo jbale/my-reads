03b9cfe0be05f54b6fa5fd8c793a8b98de5ba579
James Bale <james@buzzinteractive.co.uk>
2020-07-13 19:00:11 +0100

    test: update book cover tests

--- src/components/Book/BookCover.test.js
+++ src/components/Book/BookCover.test.js
@@ -7,3 +7,21 @@ it('renders the cover image', () => {
   const { container } = render(<BookCover cover={cover} />);
   expect(container.firstChild).toHaveStyle(`background-image: url(${cover})`)
 });
+
+it('renders no cover image when cover is empty', () => {
+  const cover = '';
+  const { container } = render(<BookCover cover={cover} />);
+  expect(container.firstChild).not.toHaveStyle(`background-image: url(${cover})`)
+});
+
+it('renders no cover image when cover is null', () => {
+  const cover = null;
+  const { container } = render(<BookCover cover={cover} />);
+  expect(container.firstChild).not.toHaveStyle(`background-image: url(${cover})`)
+});
+
+it('renders no cover image when cover is undefined', () => {
+  const cover = undefined;
+  const { container } = render(<BookCover cover={cover} />);
+  expect(container.firstChild).not.toHaveStyle(`background-image: url(${cover})`)
+});
